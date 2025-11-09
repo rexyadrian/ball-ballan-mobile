0e0897386529ee4cd95f41eb68e77e269c05e25e
rexya <rexy.adrian.f@gmail.com>
2025-11-09 21:54:54 +0700

    feat(forms): menambahkan forms produk

---
+++ lib/screens/productlist_form.dart
@@ -0,0 +1,256 @@
+import 'package:flutter/material.dart';
+import 'package:ball_ballan/widgets/left_drawer.dart';
+
+class ProductFormPage extends StatefulWidget {
+  const ProductFormPage({super.key});
+
+  @override
+  State<ProductFormPage> createState() => _ProductFormPageState();
+}
+
+class _ProductFormPageState extends State<ProductFormPage> {
+  final _formKey = GlobalKey<FormState>();
+  String _name = "";
+  int _price = 1;
+  String _category = "other"; // default
+  String _description = "";
+  String _thumbnail = "";
+  bool _isFeatured = false; // default
+
+  final List<String> _categories = [
+    'apparel',
+    'bags',
+    'shoes',
+    'equipment',
+    'accessories',
+    'other',
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Center(
+          child: Text(
+            'Add Product Form',
+          ),
+        ),
+        backgroundColor: Theme
+            .of(context)
+            .colorScheme
+            .primary,
+        foregroundColor: Colors.white,
+      ),
+      drawer: LeftDrawer(),
+      body: Form(
+        key: _formKey,
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children:[
+              // === Name ===
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: TextFormField(
+                  decoration: InputDecoration(
+                    hintText: "Product Name",
+                    labelText: "Product Name",
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(5.0),
+                    ),
+                  ),
+                  onChanged: (String? value) {
+                    setState(() {
+                      _name = value!;
+                    });
+                  },
+
+                  validator: (String? value) {
+                    if (value == null || value.isEmpty) {
+                      return "Product name cannot empty!";
+                    }
+                    return null;
+                  },
+                ),
+              ),
+
+              // === Price ===
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: TextFormField(
+                  decoration: InputDecoration(
+                    hintText: "Price",
+                    labelText: "Price",
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(5.0),
+                    ),
+                  ),
+                  keyboardType: TextInputType.number,
+                  onChanged: (String value) {
+                    setState(() {
+                      _price = int.tryParse(value) ?? 1;
+                    });
+                  },
+
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return "Price cannot empty";
+                    }
+                    int? parsed = int.tryParse(value);
+                    if (parsed == null) {
+                      return "Enter a valid number";
+                    }
+                    if (parsed <= 0) {
+                      return "Price cannot 0";
+                    }
+                    return null;
+                  },
+                ),
+              ),
+
+              // === Category ===
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: DropdownButtonFormField<String>(
+                  decoration: InputDecoration(
+                    labelText: "Category",
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(5.0),
+                    ),
+                  ),
+                  initialValue: _category,
+                  items: _categories
+                      .map((cat) => DropdownMenuItem(
+                    value: cat,
+                    child: Text(
+                        cat[0].toUpperCase() + cat.substring(1)),
+                  ))
+                      .toList(),
+                  onChanged: (String? newValue) {
+                    setState(() {
+                      _category = newValue!;
+                    });
+                  },
+                ),
+              ),
+
+              // === Description ===
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: TextFormField(
+                  maxLines: 5,
+                  decoration: InputDecoration(
+                    hintText: "About Product",
+                    labelText: "About Product",
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(5.0),
+                    ),
+                  ),
+                  onChanged: (String? value) {
+                    setState(() {
+                      _description = value!;
+                    });
+                  },
+                  validator: (String? value) {
+                    if (value == null || value.isEmpty) {
+                      return "Product description cannot empty!";
+                    }
+                    return null;
+                  },
+                ),
+              ),
+
+              // === Thumbnail URL ===
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: TextFormField(
+                  decoration: InputDecoration(
+                    hintText: "URL Thumbnail (optional)",
+                    labelText: "URL Thumbnail",
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(5.0),
+                    ),
+                  ),
+                  onChanged: (String? value) {
+                    setState(() {
+                      _thumbnail = value!;
+                    });
+                  },
+                ),
+              ),
+
+              // === Is Featured ===
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: SwitchListTile(
+                  title: const Text("Mark as Featured Product"),
+                  value: _isFeatured,
+                  onChanged: (bool value) {
+                    setState(() {
+                      _isFeatured = value;
+                    });
+                  },
+                ),
+              ),
+
+              // === Tombol Simpan ===
+              Align(
+                alignment: Alignment.bottomCenter,
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: ElevatedButton(
+                    style: ButtonStyle(
+                      backgroundColor:
+                      MaterialStateProperty.all(
+                          Theme.of(context).colorScheme.primary
+                      ),
+                    ),
+                    onPressed: () {
+                      if (_formKey.currentState!.validate()) {
+                        showDialog(
+                          context: context,
+                          builder: (context) {
+                            return AlertDialog(
+                              title: const Text('Product added!'),
+                              content: SingleChildScrollView(
+                                child: Column(
+                                  crossAxisAlignment:
+                                  CrossAxisAlignment.start,
+                                  children: [
+                                    Text('Product name: $_name'),
+                                    Text('Price: Rp$_price'),
+                                    Text('Category: $_category'),
+                                    Text('About Product: $_description'),
+                                    Text('Thumbnail: $_thumbnail'),
+                                    Text('Featured: ${_isFeatured ? "Yes" : "No"}'),
+                                  ],
+                                ),
+                              ),
+                              actions: [
+                                TextButton(
+                                  child: const Text('OK'),
+                                  onPressed: () {
+                                    Navigator.pop(context);
+                                    _formKey.currentState!.reset();
+                                  },
+                                ),
+                              ],
+                            );
+                          },
+                        );
+                      }
+                    },
+                    child: const Text(
+                      "Save",
+                      style: TextStyle(color: Colors.white),
+                    ),
+                  ),
+                ),
+              ),
+            ]
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/widgets/left_drawer.dart
@@ -0,0 +1,71 @@
+import 'package:ball_ballan/screens/productlist_form.dart';
+import 'package:flutter/material.dart';
+import 'package:ball_ballan/screens/menu.dart';
+
+class LeftDrawer extends StatelessWidget {
+  const LeftDrawer({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: ListView(
+        children: [
+          const DrawerHeader(
+            decoration: BoxDecoration(
+              color: Colors.blue,
+            ),
+            child: Column(
+              children: [
+                Text(
+                  'Ball-Ballan',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 30,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
+                  ),
+                ),
+                Padding(padding: EdgeInsets.all(10)),
+                Text(
+                  'Community Football Shop',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 15,
+                    fontWeight: FontWeight.normal,
+                    color: Colors.white,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          ListTile(
+            leading: const Icon(Icons.home_outlined),
+            title: const Text('Home'),
+            // Bagian redirection ke MyHomePage
+            onTap: () {
+              Navigator.pushReplacement(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => MyHomePage(),
+                )
+              );
+            },
+          ),
+          ListTile(
+            leading: const Icon(Icons.post_add),
+            title: const Text('Add Product'),
+            // Bagian redirection ke ProductFormPage
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (builder) => ProductFormPage()
+                ),
+              );
+            },
+          ),
+        ],
+      ),
+    );
+  }
+}
